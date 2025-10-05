59c607f503b32377f16c76f52f02ff7789004cd6
onetwothird <angelitodecatoriaa@gmail.com>
2025-10-05 14:48:10 +0800

    main initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
-import 'auth/theme.dart';
 import 'auth/login_page.dart';
+import 'roles/user/home_screen.dart';
+import 'roles/user/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.dark;
+
+  void _toggleTheme() {
+    setState(() {
+      _themeMode = _themeMode == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: AuthTheme.appTitle,
+      title: 'Seelai',
       debugShowCheckedModeBanner: false,
-      theme: AuthTheme.theme,
-      home: const LoginPage(),
-      builder: (context, child) {
-        return AuthTheme.wrapWithSystemUI(child: child!);
-      },
+      themeMode: _themeMode,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      home: LoginPage(onThemeToggle: _toggleTheme),
     );
   }
 }
